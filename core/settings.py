4d9666f8b131b7a94694b4d9dda0cc744bb6dbd8
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-10-31 19:44:23 -0300

    ajuste de versao

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ load_dotenv()  # take environment variables from .env.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # System Version
-VERSION = '1.14.50'
+VERSION = '1.14.51'
 
 # =========================== THEME CONFIGURATION ===========================
 
